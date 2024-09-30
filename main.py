0a8b29dc60c35ed92c4145d258f27822950bc20c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:04:55 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def squares_of_even_numbers():
     # TODO: Erstelle eine Liste der Quadrate aller geraden Zahlen von 1 bis 100
-    return [i ** 2 for i in range(2, 102, 2)]
+    return [i**2 for i in range(2, 102, 2)]
 
 
 if __name__ == '__main__':
